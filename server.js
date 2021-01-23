1f3d8fc19a10379aba0104cb455c978bade5afb0
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-01-23 15:14:39 -0800

    Finished Boilerplate Code
    This commit finishes the boilerplate code necessary to get  NodeJS to actually listen for endpoints.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 
 // This is the main API endpoint for getting crypto values from tokens
 app.get('/api/:uid', (req, res) => {
-    
+    let user_id = req.params.uid;
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
