b1afa56cb4957f0465fadffa10395019df997da5
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-01-23 15:10:57 -0800

    Added Initial Get Endpoint
    This commit adds the initial get endpoint at /api/:uid.

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 const express = require('express');
+const app = express();
 
+// This is the main API endpoint for getting crypto values from tokens
+app.get('/api/:uid', (req, res) => {
+    
+});
