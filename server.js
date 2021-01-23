0a90e74998680ad6503813a4cff50db9001fcc61
Karthik Dharmarajan <karthikdharmarajan123@gmail.com>
2021-01-23 15:22:40 -0800

    Added Get Request for User Tokens
    This commit adds it so that the backend gets the amount of tokens a user has.

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 const port = 8080;
 
 // This is the main API endpoint for getting crypto values from tokens
 app.get('/api/:uid', (req, res) => {
     let user_id = req.params.uid;
+    https.get(`https://stg-api.tegger.io/api/v1/interactions/history/${user_id}`, (res) => {
+        let data = "";
+        res.on("data", new_data => {
+            data += new_data;
+        });
+    });
 });
 
 app.listen(port, () => {
